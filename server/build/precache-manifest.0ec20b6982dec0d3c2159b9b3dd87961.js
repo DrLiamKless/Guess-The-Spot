684561fd3c8699030f8c7f5e0ddea6c16a72f26a
DrLiamKless <67854560+DrLiamKless@users.noreply.github.com>
2020-11-18 23:04:27 +0200

    build

---
+++ server/build/precache-manifest.0ec20b6982dec0d3c2159b9b3dd87961.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8408c09c8866dbf8f133e67180b57a8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd6d44a328863d22b867",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1ab0bd2e3e6046e1929d",
+    "url": "/static/css/main.d4aa151d.chunk.css"
+  },
+  {
+    "revision": "bd6d44a328863d22b867",
+    "url": "/static/js/2.dea7c314.chunk.js"
+  },
+  {
+    "revision": "b962909a7040b53b76bb931dd59e4242",
+    "url": "/static/js/2.dea7c314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab0bd2e3e6046e1929d",
+    "url": "/static/js/main.1d81caf1.chunk.js"
+  },
+  {
+    "revision": "5cec0d85f9b87f77aa14",
+    "url": "/static/js/runtime-main.4162e8ee.js"
+  },
+  {
+    "revision": "5676aaa96f00056841d9f6185951b1b1",
+    "url": "/static/media/BG.5676aaa9.jpg"
+  }
+]);
